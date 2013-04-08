452f7d138469585e9b3ffa245bf0e1b11953a6f0
Casey Link <unnamedrambler@gmail.com>
2013-04-08 20:20:30 +0200

    button for launching the script

--- src/com/binaryelysium/ephemvpn/MainActivity.java
+++ src/com/binaryelysium/ephemvpn/MainActivity.java
@@ -1,15 +1,32 @@
 package com.binaryelysium.ephemvpn;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener( new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ScriptActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
     }
 
 
@@ -18,6 +35,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
