7f3847e7ac8094a1a34afb258804e5a5a910b5ce
Casey Link <unnamedrambler@gmail.com>
2013-04-08 14:29:32 +0200

    install our bundle in the app and invoke python from java

--- external/ephemvpn_android.py
+++ external/ephemvpn_android.py
@@ -1,5 +1,45 @@
 import sys
 import os
-#sys.path.append(os.getcwd()+'/libs')
+import android
 
-import ephemvpn
+sys.path.append(os.getcwd()+'/libs')
+droid = android.Android()
+
+error = ''
+
+try:
+    import sys
+    import argparse
+    import getpass
+    import os
+    import time
+    import datetime
+    import math
+    import ConfigParser
+except ImportError, e:
+    error += "import 1 failed\n"
+    droid.log(e)
+
+try:
+    import parsedatetime
+except ImportError, e:
+    error += "import 2 failed\n"
+    droid.log(e)
+
+try:
+    import ephemvpn
+    from ephemvpn import configuration as config
+    from ephemvpn import vpntypes, launch, configure
+    from ephemvpn.timehelpers import readable_delta
+except ImportError, e:
+    error += "import 3 failed\n"
+    droid.log(e)
+
+
+if error == '':
+    droid.makeToast("ephemvpn A OK !")
+    droid.log("ephevpn A  OK!!")
+else:
+    droid.makeToast(error)
+    droid.log(error)
+
